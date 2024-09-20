55c457e1d2122b912bfffb2444cdf13c39cd1ee6
Marco Colombo <ing.marco.colombo@gmail.com>
2024-09-20 16:29:38 +0200

    More tests

--- business/entityalign/align_test.go
+++ business/entityalign/align_test.go
@@ -82,13 +82,72 @@ func TestAlign_AlignAlreadyCreatedModelsIfNeeded(t *testing.T) {
 	swclient.On("PollForModelActiveStatus", ctx, modelId, mock.Anything).Return(true)
 
 	models := make(map[string]*string)
+	models["temperature"] = toPtr(modelId)
+	uomMap := make(map[string][]string)
+
+	aligner := New(swclient, logger)
+	_, errs := aligner.alignAlreadyCreatedModels(ctx, thingsMap, models, modelDefinitions, assets, uomMap)
+	assert.Nil(t, errs)
+	assert.Equal(t, 2, len(models))
+	assert.Equal(t, modelId, *models["pressure,temperature"])
+}
+
+func TestAlign_IfAlignedNoCallsToSW(t *testing.T) {
+
+	ctx := context.Background()
+	logger := logrus.NewEntry(logrus.New())
+
+	// Static id definitions
+	thingId := "bb831f04-0940-4ea6-9c24-83668e372919"
+	modelId := "03ba45c2-eab3-44ed-a68f-94a26d41df4c"
+
+	// Mocks
+	swclient := sitewiseMocks.NewAPI(t)
+
+	// Define thing
+	thingsMap := make(map[string]iotclient.ArduinoThing)
+	thingsMap[thingId] = iotclient.ArduinoThing{
+		Id: thingId,
+		Properties: []iotclient.ArduinoProperty{
+			{
+				Name: "temperature",
+				Type: "INT",
+			},
+		},
+	}
+
+	modelDefinitions := make(map[string]*iotsitewise.DescribeAssetModelOutput)
+	modelDefinitions[modelId] = &iotsitewise.DescribeAssetModelOutput{
+		AssetModelId: &modelId,
+		AssetModelProperties: []types.AssetModelProperty{
+			{
+				DataType: types.PropertyDataTypeDouble,
+				Name:     toPtr("temperature"),
+				Type: &types.PropertyType{
+					Measurement: &types.Measurement{},
+				},
+			},
+		},
+	}
+
+	// Define asset
+	assets := make(map[string]assetDefintion)
+	assets[thingId] = assetDefintion{
+		assetId: "e9e11559-ceca-4c2f-875d-76c1068a45f4",
+		modelId: modelId,
+		thingId: thingId,
+	}
+
+	// No calls to SW
+
+	models := make(map[string]*string)
+	models["temperature"] = &modelId
 	uomMap := make(map[string][]string)
 
 	aligner := New(swclient, logger)
 	_, errs := aligner.alignAlreadyCreatedModels(ctx, thingsMap, models, modelDefinitions, assets, uomMap)
 	assert.Nil(t, errs)
 	assert.Equal(t, 1, len(models))
-	assert.Equal(t, modelId, *models["pressure,temperature"])
 }
 
 func toPtr(val string) *string {
